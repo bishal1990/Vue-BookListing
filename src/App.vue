339408d876b0a6abf441311e7e2d5ecdbc9547f7
Bishal Sapkota <sapkotabishal1990@gmail.com>
2019-02-17 14:04:03 -0500

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+<<<<<<< HEAD
     <book-list></book-list>
+=======
+    <hello-world></hello-world>
+>>>>>>> parent of 719d2ef... this is initial commit
   </div>
 </template>
 
 <script>
-import BookList from './components/BookList';
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'app',
   components: {
+<<<<<<< HEAD
     BookList, 
+=======
+    HelloWorld,
+>>>>>>> parent of 719d2ef... this is initial commit
   },
 };
 </script>
